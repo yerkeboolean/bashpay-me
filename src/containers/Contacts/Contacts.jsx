87cc2fa6a1e751acdf927d29428442c9cc2e2b46
Yerkebulan <160103106@stu.sdu.edu.kz>
2021-01-15 00:55:10 +0600

    feat: layout contact page

--- src/containers/Contacts/Contacts.jsx
+++ src/containers/Contacts/Contacts.jsx
@@ -1,9 +1,25 @@
-import React, { Component } from 'react';
-import './Contacts.css';
+import React, { Component } from "react";
+import classes from "./Contacts.module.css";
 
 class Contacts extends Component {
   render() {
-    return <div className="contacts">Contacts</div>;
+    return (
+      <div className={classes.Contacts}>
+        <div className={classes.header}>Contacts</div>
+        <div className={classes.phone}>
+          Telegram/Whats app:{" "}
+          <span>
+            <a href="tel:+77783088200">+7 778 308 82 00</a>
+          </span>
+        </div>
+        <div className={classes.email}>
+          Mail:
+          <span>
+            <a href="mailto:98ernur@gmail.com"> 98ernur@gmail.com</a>
+          </span>
+        </div>
+      </div>
+    );
   }
 }
 
